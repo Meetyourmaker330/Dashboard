6714caa84b51970cf9198e00f3801cfaec4ce2d0
Meetyourmaker330 <72625202+Meetyourmaker330@users.noreply.github.com>
2021-05-27 14:48:09 +0300

    change js file

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,6 @@ let burger = document.getElementById('burger-menu');
 let mobileMenu = document.getElementById('mob-menu');
 
 burger.addEventListener('click', () => {
-   document.body.classList.toggle('lock');
    burger.classList.toggle('active');
    mobileMenu.classList.toggle('active');
-
-   console.log('hello')
 });
