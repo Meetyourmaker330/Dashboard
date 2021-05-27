3ea109a8ae7b335013d295f2740e3dc4e7b691ee
Meetyourmaker330 <72625202+Meetyourmaker330@users.noreply.github.com>
2021-05-27 19:24:45 +0300

    add dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 let burger = document.getElementById('burger-menu');
 let mobileMenu = document.getElementById('mob-menu');
+let dropDownBtn = document.getElementById('open-dropdown-btn');
+let dropdown = document.getElementById('dropdown');
 
 burger.addEventListener('click', () => {
    burger.classList.toggle('active');
    mobileMenu.classList.toggle('active');
 });
+
+dropDownBtn.addEventListener('click', () => {
+   dropDownBtn.classList.toggle('show');
+   dropdown.classList.toggle('show');
+})
+
+
